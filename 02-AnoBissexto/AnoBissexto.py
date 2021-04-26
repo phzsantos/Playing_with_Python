193da58e519fbe16d68640c69aec5c9425d7d3cd
phzsantos <phzsantos2002@gmail.com>
2021-04-25 23:16:04 -0300

    Consertando a logica do programa de AnoBissexto e otimizando o codigo

--- 02-AnoBissexto/AnoBissexto.py
+++ 02-AnoBissexto/AnoBissexto.py
@@ -4,21 +4,13 @@ print('Programa para saber se o ano é bissexto ou não.\n')
 
 #------------------------------------ REGRA ------------------------------------
 
-eEnt1 = int(input('diga-me um ano, eu te direi se ele é bissexto ou não: \n'))
+Ano = int(input('diga-me um ano, eu te direi se ele é bissexto ou não: '))
 
-if eEnt1 < 104:
+if (Ano < 104):
 	print('Antes do ano 104 d.c, não tinhamos anos bissexto. Portanto, o numero digitado não é um ano bissexto')
 
-elif (eEnt1 % 4) == 0:
-	print('Esse ano é bissexto')
-	
-elif (eEnt1 % 100) == 0:
-
-	if (eEnt1 % 400) == 0:
-		print('Esse ano é bissexto, pois atende todos os requisitos')
-
-	else:
-		('Esse ano não é bissexto, pois não é divisivel por 400')
+elif ((Ano % 400) == 0) or ((Ano % 4) == 0) and ((Ano % 100) != 0):
+	print('Esse ano é bissexto.')
 
 else:
 	print('Esse ano não é bissexto.')
