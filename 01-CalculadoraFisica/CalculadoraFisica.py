09896b489292b9be2ebb524f2e8e6d16d3d1709d
phzsantos <phzsantos2002@gmail.com>
2021-04-26 23:54:31 -0300

    Melhorando nome das variaveis

--- 01-CalculadoraFisica/CalculadoraFisica.py
+++ 01-CalculadoraFisica/CalculadoraFisica.py
@@ -34,10 +34,10 @@ print('4-Calor Latente\n')
 print('5-Calor Sensivel + Latente\n')
 print('6-Gases Ideais\n')
 print('Qual o tema a ser abordado?(responda de 1 a 6)')
-p1=input()
+OpcaoMenu=input()
 
 #PARTE-2-3-4 (TEMPERATURA)
-if p1=="1":
+if OpcaoMenu=="1":
 	print('\nVocê escolheu temperatura, aqui estão as alternativas:\n')
 	print('1-Celsius para Kelvin\n')
 	print('2-Celsius para Fahrenheit\n')
@@ -47,8 +47,8 @@ if p1=="1":
 	print('6-Kelvin para Fahrenheit\n')
 	print('7-Variação de temperatura\n')
 	print('Qual a conversão que deseja fazer?(responda com 1 ate 7)')
-	p2=input("")
-	if p2=="1":
+	TipoConversaoTemperatura=input("")
+	if TipoConversaoTemperatura=="1":
 		print('\nTemperatura é uma grandeza microscopica, que é a média das velocidades das moleculas ou atomos de determinado corpo\n')
 		print('Formula: Tc/5=Tk-273/5 ou Resumindo, fica o valor em Celsius + 273\n')
 		print('valores da temperatura humana em:')
@@ -58,7 +58,7 @@ if p1=="1":
 		valorcelsius=float(input('Qual o valor em Celsius?'))
 		contacelsiuskelvin=valorcelsius+273
 		print(f'Seu valor de Celsius convertido para Kelvin fica {contacelsiuskelvin} K')
-	elif p2=="2":
+	elif TipoConversaoTemperatura=="2":
 		print('\nTemperatura é uma grandeza microscopica, que é a média das velocidades das moleculas ou atomos de determinado corpo\n')
 		print('Formula: Tf=(Tc*9/5)+32\n')
 		print('valores da temperatura humana em:')
@@ -68,7 +68,7 @@ if p1=="1":
 		valorcelsius2=float(input('Qual o valor em Celsius?'))
 		contacelsiusfahrenheit=(valorcelsius2*9/5)+32
 		print(f'Seu valor de Celsius convertido para Fahrenheit fica {contacelsiusfahrenheit} ºF')
-	elif p2=="3":
+	elif TipoConversaoTemperatura=="3":
 		print('\nTemperatura é uma grandeza microscopica, que é a média das velocidades das moleculas ou atomos de determinado corpo\n')
 		print('Formula: Tc=(Tf-32)*5/9\n')
 		print('valores da temperatura humana em:')
@@ -78,7 +78,7 @@ if p1=="1":
 		valorfahrenheit=float(input('Qual o valor em Fahrenheit?'))
 		contafahrenheitcelsius=(valorfahrenheit-32)*5/9
 		print(f'Seu valor de Fahrenheit convertido para Celsius fica {contafahrenheitcelsius} ºC')
-	elif p2=="4":
+	elif TipoConversaoTemperatura=="4":
 		print('\nTemperatura é uma grandeza microscopica, que é a média das velocidades das moleculas ou atomos de determinado corpo\n')
 		print('Formula: Tk=(Tf-32)*5/9+273\n')
 		print('valores da temperatura humana em:')
@@ -88,7 +88,7 @@ if p1=="1":
 		valorfahrenheit2=float(input('Qual o valor em Fahrenheit?'))
 		contafahrenheitkelvin=(valorfahrenheit2-32)*5/9+273
 		print(f'Seu valor de Fahrenheit convertido para Kelvin fica {contafahrenheitkelvin} K')
-	elif p2=="5":
+	elif TipoConversaoTemperatura=="5":
 		print('\nTemperatura é uma grandeza microscopica, que é a média das velocidades das moleculas ou atomos de determinado corpo\n')
 		print('Formula: Tc=Tk-273\n')
 		print('valores da temperatura humana em:')
@@ -98,7 +98,7 @@ if p1=="1":
 		valorkelvin=float(input('Qual o valor em Kelvin?'))
 		contakelvincelsius=valorkelvin-273
 		print(f'Seu valor de Fahrenheit convertido para Kelvin fica {contakelvincelsius} ºC')
-	elif p2=="6":
+	elif TipoConversaoTemperatura=="6":
 		print('\nTemperatura é uma grandeza microscopica, que é a média das velocidades das moleculas ou atomos de determinado corpo\n')
 		print('Formula: Tf=(Tk-273)*9/5+32\n')
 		print('valores da temperatura humana em:')
@@ -110,39 +110,39 @@ if p1=="1":
 		print(f'Seu valor de Fahrenheit convertido para Kelvin fica {contakelvinfahrenheit} ºF')	
 
 #PARTE-2-3-4 (DILATAÇÃO TERMICA)		
-elif p1=="2":
+elif OpcaoMenu=="2":
 	print('ainda esta em construção.')
 	print('Você escolheu Dilatação Termica\n')
 	print('Qual sua icognita?(responda com )')
 	p3=input()
 
 #PARTE-2-3-4 (CALOR SENSIVEL)	
-elif p1=="3":
+elif OpcaoMenu=="3":
 	print('\nVocê escolheu Calor Sensivel\n')
 	print('Qual sua icognita?(responda com Q,M,C ou Delta0)')
-	p4=input()
-	if p4=="Q":
+	CalorSensivelIcognita=input()
+	if CalorSensivelIcognita=="Q":
 		print('\nEssa é a formula usada: Q=m.c.delta0\n')
 		M=float(input('Qual é sua M?(sempre em gramas)\n'))
 		C=float(input('Qual é seu Calor especifico?\n'))
 		delta0=float(input('Qual é sua variação de temperatura?(temperatura final - inicial)\n'))
 		contaequiQ=M*C*delta0
 		print(f'O valor de Q é {contaequiQ} + unidade da sua questão')
-	elif p4=="M":
+	elif CalorSensivelIcognita=="M":
 		print('\nEssa é a formula usada: Q=m.c.delta0\n')
 		Q=float(input('Qual é sua Q?\n'))
 		C=float(input('Qual é seu Calor especifico?\n'))
 		delta0=float(input('Qual é sua variação de temperatura?(temperatura final - inicial)\n'))
 		contaequiM=Q/(C*delta0)
 		print(f'O valor de Q é {contaequiM} + unidade da sua questão')
-	elif p4=="C":
+	elif CalorSensivelIcognita=="C":
 		print('\nEssa é a formula usada: Q=m.c.delta0\n')
 		Q=float(input('Qual é sua Q?\n'))
 		M=float(input('Qual é sua M?(sempre em gramas)\n'))
 		delta0=float(input('Qual é sua variação de temperatura?(temperatura final - inicial)\n'))	
 		contaequiC=Q/(M*delta0)
 		print(f'O valor de Q é {contaequiC} + unidade da sua questão')
-	elif p4=="Delta0":
+	elif CalorSensivelIcognita=="Delta0":
 		final_ou_inicial=input('\nA temperatura que você tem é final ou inicial?\n')
 		if final_ou_inicial=="inicial":
 			print('\nEssa é a formula usada: Q=m.c.delta0\n')
@@ -164,23 +164,23 @@ elif p1=="3":
 			print(f'O valor de Q é {contaequiDfinal2} + unidade da sua questão')
 
 #PARTE-2-3-4 (CALOR LATENTE)
-elif p1=="4":
+elif OpcaoMenu=="4":
 	print('\nVocê escolheu Calor Latente\n')
 	print('Qual sua icognita?(responda com Q,M ou L)')
-	p5=input()
-	if p5=="Q":
+	CalorLatenteIcognita=input()
+	if CalorLatenteIcognita=="Q":
 		print('\nEssa é a formula usada: Q=m.l\n')
 		M=float(input('Qual é sua M?(sempre em gramas)\n'))
 		L=float(input('Qual é seu valor de L?\n'))
 		contalatQ=M*L
 		print(f'O valor de Q é {contalatQ} + unidade da sua questão')
-	elif p5=="M":
+	elif CalorLatenteIcognita=="M":
 		print('\nEssa é a formula usada: M=Q/l\n')
 		Q=float(input('Qual é o valor do seu Q?\n'))
 		L=float(input('Qual é seu valor de L?\n'))
 		contalatM=Q/L
 		print(f'O valor de M é {contalatM} + unidade da sua questão')
-	elif p5=="L":
+	elif CalorLatenteIcognita=="L":
 		print('\nEssa é a formula usada: L=Q/m\n')
 		Q=float(input('Qual é o valor do seu Q?\n'))
 		M=float(input('Qual é sua M?(sempre em gramas)\n'))
@@ -188,13 +188,13 @@ elif p1=="4":
 		print(f'O valor de L é {contalatL} + unidade da sua questão')
 
 #PARTE-2-3-4 (CALOR SENSIVEL + LATENTE)
-elif p1=="5":
+elif OpcaoMenu=="5":
 	print('\nVocê escolheu Calor Senvivel + Latente\n')
 	print('Qual a sua icognita? (responda com Q,M,C ou Delta0)')
-	p6=input()
+	CalorSensivelLatenteIcognita=input()
 
 #PARTE-2-3-4 (Gases Ideais)
-elif p1=="6":
+elif OpcaoMenu=="6":
 	print('\nVocê escolheu Gases ideais\n')
 	print('como o desenvolvedor é preguiçoso, ele ainda não trabalhou na parte matematica e logica do bagulho, no entanto aqui estão alguns axiomas:\n')
 	print('Axioma 0:')
@@ -215,4 +215,4 @@ elif p1=="6":
 else:
 	print('\nA opção que você escolheu não existe.')
 	print('Você quer tentar novamente? [S/N]')
-	p0=input()
+	FinalPrograma=input()
